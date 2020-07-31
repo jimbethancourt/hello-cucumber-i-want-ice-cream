d473904c5c0832cc75153551b0f13b83f64603de
Jim Bethancourt <jimbethancourt@gmail.com>
2020-07-31 10:19:50 -0500

    Added Cluecumber plugin

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -6,6 +6,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-report/cucumber.json"})
 public class RunCucumberTest {
 }
